b11b67afe8bffb105b8786c0184ec1abdf311f75
Roman Kuzmichev <yetikotuch@yandex.ru>
2022-01-15 18:28:43 +0300

    refactor path logic

--- sylladex/accomodate.js
+++ sylladex/accomodate.js
@@ -2,14 +2,25 @@ import { v4 as uuid } from "uuid";
 import * as fs from "fs/promises";
 import _ from "lodash/fp.js";
 
-const accomodate = async (file, fileParams, params) => {
-	const path = uuid();
-	const fullPath = params.persistence + '/' + path;
-	await fs.mkdir(fullPath);
-	await file.mv(fullPath + '/file');
-	const metaData = _.pick(['name', 'size', 'encoding', 'mimetype', 'md5'])(file);
-	await fs.writeFile(fullPath + '/meta.json', JSON.stringify(metaData));
-	return path;
+class FileStorage {
+	#config;
+	constructor(config) {
+		this.config = config;
+	};
+	async put(file) {
+		const path = uuid();
+		const fullPath = this.config.persistencePath + '/' + path;
+		await fs.mkdir(fullPath);
+		await file.mv(fullPath + '/file');
+		const metaData = _.pick(['name', 'size', 'encoding', 'mimetype', 'md5'])(file);
+		await fs.writeFile(fullPath + '/meta.json', JSON.stringify(metaData));
+		return path;
+	};
+	getFilename(name) {return this.config.persistencePath + '/' + name + '/file';}
+	getMetaname(name) {return this.config.persistencePath + '/' + name + '/meta.json';}
+	async getMeta(name) {return fs.readFile(this.getMetaname(name)).then(JSON.parse);};
+
 };
 
-export default accomodate;
+
+export default FileStorage;
--- sylladex/index.js
+++ sylladex/index.js
@@ -1,44 +1,41 @@
 import express from "express";
 import fileUpload from "express-fileupload";
 import _ from "lodash/fp.js";
-import accomodate from "./accomodate.js";
+import FileStorage from './accomodate.js';
 import * as fs from "fs/promises";
 
 const app = express();
 app.use(fileUpload());
 
+const files = new FileStorage({persistencePath:'/persistence'});
+
+// TODO investigate express + promises, maybe streamline it all a bit
 app.get('/v1/meta/:name', (req, res) => {
-	// TODO: check that name looks plausible
-	const metaName = '/persistence/' + req.params.name + '/meta.json';
-	fs.readFile(metaName)
-		.then((data) => res.send(JSON.parse(data)))
+	files.getMeta(req.params.name)
+		.then(res.send(data))
 		.catch(() => res.send("404 not found"))// TODO: proper machine friendly 404
 });
 
 app.get('/v1/file/:name', (req, res) => {
-	// TODO: check that name looks plausible
-	const filename = '/persistence/' + req.params.name + '/file';
-	const metaName = '/persistence/' + req.params.name + '/meta.json';
-	fs.readFile(metaName)
-		.then(JSON.parse)
+	files.getMeta(req.params.name)
 		.then((data) => {
 			res.type(data.mimetype);
 			res.attachment(data.name);
 		})
-		.then(() => res.sendFile(filename))
+		.then(() => res.sendFile(files.getFilename(req.params.name)))
 		.catch(() => res.send("404 not found"))// TODO: proper machine friendly 404
 });
 
 app.post('/v1/upload', (req, res) => {
-	const files = _.values(req.files);
-	if (!files || files.length != 1) {
+	const newFiles = _.values(req.files);
+	if (!newFiles || newFiles.length != 1) {
 		return res.send({
 			success: false,
 			error: true,
 			errorMessage: "You have to post exactly 1 file"
 		});
 	}
-	accomodate(files[0], {}, {persistence: '/persistence'})
+	files.put(newFiles[0])
 		.then((path) => res.send({success: true, error: false, path}))
 });
 
